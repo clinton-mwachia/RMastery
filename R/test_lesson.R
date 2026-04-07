219e49ad8742deb18452c69a2786f87fe5a1e4dd
Clinton Moshe <clintonmwachia9@gmail.com>
2026-04-07 17:22:37 +0300

    Test Github installation

---
+++ R/test_lesson.R
@@ -0,0 +1,7 @@
+library(RMastery)
+
+# browse available tutorials
+learnr::available_tutorials("RMastery")
+
+# lauch a given lesson
+learnr::run_tutorial(name = "introduction", package = "RMastery")
