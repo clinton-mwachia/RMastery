14f933c9d3015eb4718addc6839d4661a59e1f93
Clinton Moshe <clintonmwachia9@gmail.com>
2026-04-08 20:19:06 +0300

    Update NAMESPACE

--- R/test_lesson.R
+++
@@ -1,7 +0,0 @@
-library(RMastery)
-
-# browse available tutorials
-learnr::available_tutorials("RMastery")
-
-# lauch a given lesson
-learnr::run_tutorial(name = "introduction", package = "RMastery")
