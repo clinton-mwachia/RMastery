ba52070ed6f541945dd3bffd54660a42b804380b
Clinton Moshe <clintonmwachia9@gmail.com>
2026-04-08 08:35:08 +0300

    Added functions to lauch the lessons: introduction, data structures and conditionals statements

--- R/launch_lesson.R
+++ R/launch_lesson.R
@@ -1,1 +1,7 @@
+learnr::run_tutorial("F:/R/RMastery/inst/tutorials/introduction/introduction.Rmd")
+
+learnr::run_tutorial("F:/R/RMastery/inst/tutorials/data_structures/data_structures.Rmd")
+
+learnr::run_tutorial("F:/R/RMastery/inst/tutorials/conditional_statements/conditional_statements.Rmd")
+
 learnr::run_tutorial("F:/R/RMastery/inst/tutorials/functions/functions.Rmd")
